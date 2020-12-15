2a2e9d869573998e5d2bed1b9cb81849bf12597b
mok24112 <m.nurth.pcef424@gmail.com>
2020-12-15 17:50:45 +0900

    add file

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,19 +1,30 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-
 
 @Controller
 public class MainController {
-@RequestMapping("/")
-//public ModelAndView index(ModelAndView mv){
-//mv.setViewName("index");
-//return mv;
-public String index(Model model){
-model.addAttribute("name", "鈴木Java五郎");
-return "index";
+@RequestMapping(value="/day20", method=RequestMethod.GET)
+public ModelAndView indexGet(ModelAndView mv){
+mv.addObject("name", "名前がここに入ります");
+mv.addObject("age", "年齢がここに入ります");
+mv.addObject("height", "身長がここに入ります");
+mv.setViewName("day20");
+return mv;
+}
+
+@RequestMapping(value="/day20", method=RequestMethod.POST)
+public ModelAndView indexPost(ModelAndView mv, @RequestParam("nameVal")String
+name,
+@RequestParam("ageVal")String age, @RequestParam("heightVal")String height){
+mv.addObject("name", name);
+mv.addObject("age", age);
+mv.addObject("height", height);
+mv.setViewName("day20");
+return mv;
 }
 }
